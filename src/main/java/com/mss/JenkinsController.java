42645527e6d4601c6fb171eedeadecb4ad561eff
PavaniReddy <93965750+18Jn1a0423@users.noreply.github.com>
2024-07-15 15:38:14 +0530

    Update JenkinsController.java

--- src/main/java/com/mss/JenkinsController.java
+++ src/main/java/com/mss/JenkinsController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class JenkinsController {
     @GetMapping("/welcome")
     public String welcome1() {
-        return "<html><head><style>body { display: flex; justify-content: center; align-items: center; height: 100vh;}</style></head><body><h1 style='color:red; text-align:center;'>WELCOME TO MY <b>DEPLOYMENT<b><br></h1></body></html>";
+        return "<html><head><style>body { display: flex; justify-content: center; align-items: center; height: 100vh;}</style></head><body><h1 style='color:blue; text-align:center;'>WELCOME TO MY <b>DEPLOYMENT<b><br></h1></body></html>";
     }
 }
 
